eee4edca2c80f6cc8724d6bec39b23d7e8bab0eb
Shivaguru S <shivagurus2452@gmail.com>
2026-02-06 23:06:39 +0530

    Updated server.js for deployment

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
 const connectDB = require("./config/db");
 
 const transactionRoutes = require("./routes/transactionRoutes");
 const accountRoutes = require("./routes/accountRoutes");
 
-connectDB();
-
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
+
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Money Manager Backend API is running 🚀");
+});
 
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/accounts", accountRoutes);
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
